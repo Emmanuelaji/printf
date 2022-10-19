0946a0a01a3e404754cf2bbebcf4fc93a1f5a110
EMMANUEL AJI AGBO <31956253+Emmanuelaji@users.noreply.github.com>
2022-10-18 22:59:51 -0400

    Functions that calculate active flags

--- get_flags.c
+++ get_flags.c
@@ -1,14 +1,11 @@
 #include "main.h"
 
 /**
-* get_flags - Function that calculates active flags
-*
-* @format: Formatted string in which to print the arguments
-*
-* @i: take a parameter.
-*
-* Return: returns Flags:
-*/
+ * get_flags - Calculates active flags
+ * @format: Formatted string in which to print the arguments
+ * @i: take a parameter.
+ * Return: Flags:
+ */
 int get_flags(const char *format, int *i)
 {
 /* - + 0 # ' ' */
